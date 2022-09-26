1b9239b3ca691653dc0e8f8a92101eda1cc0adde
Bae Sujeong <tnwjd_@naver.com>
2022-09-26 09:36:15 +0900

    모든 게시물을 가져오는 Service 만들기

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class BoardsService {}
+export class BoardsService {
+    //front에서 수정하는걸 방지하기 위한 private(접근제한자)사용
+    private boards = [];
+
+    getAllBoards() {
+        return this.boards;
+    }
+}
